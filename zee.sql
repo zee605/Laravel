7ffc35a00cada9907088563284e07036460cf291
zee605 <128007835+zee605@users.noreply.github.com>
2024-02-03 18:36:15 +0700

    Update zee.sql

--- zee.sql
+++ zee.sql
@@ -41,20 +41,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `alamat`, `notlp`, `foto`) VALUES
-(13, 'testing update sweet', 'imhuman@mars.com', 'living on earth', '6', ''),
-(14, 'coba', '605iksanmaulana@gmail.com', 'living on earth', '7', ''),
-(15, 'zeeroy', 'blaa@gmail.comas', 'living on earth', '8', ''),
-(16, 'zeeroy', '605iksanmaulana@gmail.com', 'living on earth', '9', ''),
-(17, 'zeeroy', 'blaa@gmail.comas', 'living on earth', '10', ''),
-(18, 'iksan maulana', 'imhuman@mars.com', 'in living in my dream', '088888', ''),
 (19, 'coba upload foto', 'blaa@gmail.comas', 'living on earth', '1', 'biGSuzOYHa6wkvLCTSDrnOC4EWt0l90aX0LiqXCS.jpg'),
-(20, 'iksan maulana', 'blaa@gmail.comas', 'living on earth', '2', 'I01Lr5kr7ld9jLPSb6uAfLPqMXI8yQjnpbkbd0xs.jpg'),
-(21, 'zeeroy', '605iksanmaulana@gmail.com', 'living on earth', '3', 'waJtYus22KGaiyjRPPaMQvXRTF8ckTvpElb7WZ7D.jpg'),
-(22, 'zeeroy', '605iksanmaulana@gmail.com', 'living on earth', '3', 'kZXI3ChHPbYFkUfVHDZglqSZWUVztRx9siaL0WSZ.jpg'),
-(23, 'zeeroy', '605iksanmaulana@gmail.com', 'living on earth', '4', 'QjL4nQsdVK5rymcCkImHDrSWRhgRrJZUy4Eh4YMd.jpg'),
-(24, 'zeeroy', '605iksanmaulana@gmail.com', 'living on earth', '5', 'sHVA0tZLNAz6yK0cQd8NyJoNedO7jDXvmkpqUCD4.jpg'),
-(25, 'iksan maulana', 'iksan 605', 'hello', '605', 'UyDNc1TDdkyYjoQMlRFSqCvrSmZhExYFvQAPQI6E.jpg'),
-(26, '<h1>hello</h1>', 'iksan 605', 'living on earth', '123', 'l10Eu5v1U2La3CI3TIcXMBqYPiNk5Zo6uHKQpBIa.jpg');
 
 --
 -- Indexes for dumped tables
